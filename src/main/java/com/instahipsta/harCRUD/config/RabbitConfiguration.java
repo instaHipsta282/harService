252e3f8d0ffc0974f72d3a96076c39199803157b
instaHipsta282 <stepaden@mail.ru>
2020-01-13 16:43:15 +0300

    remove unused configuration

--- src/main/java/com/instahipsta/harCRUD/config/RabbitConfiguration.java
+++ src/main/java/com/instahipsta/harCRUD/config/RabbitConfiguration.java
@@ -14,42 +14,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitConfiguration {
 
-    @Value("${spring.rabbitmq.host}")
-    private String host;
-    @Value("${spring.rabbitmq.username}")
-    private String username;
-    @Value("${spring.rabbitmq.password}")
-    private String password;
-
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host);
-        connectionFactory.setUsername(username);
-        connectionFactory.setPassword(password);
-        return connectionFactory;
-    }
-
-    @Bean
-    public AmqpAdmin amqpAdmin() {
-        return new RabbitAdmin(connectionFactory());
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate() {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
-        rabbitTemplate.setExchange("harExchange");
-        return rabbitTemplate;
-    }
-
     @Bean
     public Queue harQueue() {
         return new Queue("harQueue");
     }
 
     @Bean
-    public DirectExchange directExchange() {
-        return new DirectExchange("harExchange");
-    }
+    public DirectExchange directExchange() { return new DirectExchange("harExchange"); }
 
     @Bean
     public Binding harBinding() {
