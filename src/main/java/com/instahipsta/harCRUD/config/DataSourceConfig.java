9d3412583286b26d2ae69fb0f9ee5bdb4a94c7a5
instaHipsta282 <stepaden@mail.ru>
2020-01-13 10:07:53 +0300

    clean pom.xml, remove second manifest.mf and datasource configuration file

--- src/main/java/com/instahipsta/harCRUD/config/DataSourceConfig.java
+++ src/main/java/com/instahipsta/harCRUD/config/DataSourceConfig.java
@@ -1,31 +1,31 @@
-package com.instahipsta.harCRUD.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class DataSourceConfig {
-
-    @Value("${spring.datasource.driverClassName}")
-    private String driver;
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${spring.datasource.username}")
-    private String username;
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Bean
-    public DataSource getDataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName(driver);
-        dataSourceBuilder.url(url);
-        dataSourceBuilder.username(username);
-        dataSourceBuilder.password(password);
-        return dataSourceBuilder.build();
-    }
-}
+//package com.instahipsta.harCRUD.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class DataSourceConfig {
+//
+//    @Value("${spring.datasource.driverClassName}")
+//    private String driver;
+//    @Value("${spring.datasource.url}")
+//    private String url;
+//    @Value("${spring.datasource.username}")
+//    private String username;
+//    @Value("${spring.datasource.password}")
+//    private String password;
+//
+//    @Bean
+//    public DataSource getDataSource() {
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.driverClassName(driver);
+//        dataSourceBuilder.url(url);
+//        dataSourceBuilder.username(username);
+//        dataSourceBuilder.password(password);
+//        return dataSourceBuilder.build();
+//    }
+//}
