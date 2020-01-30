34d0ba82ab7094ef3fe510b567c48e94121dfe57
instaHipsta282 <stepaden@mail.ru>
2020-01-30 10:04:35 +0300

    Change crud to JpaRepository and remove unused method

--- src/main/java/com/instahipsta/harCRUD/repository/HarRepo.java
+++ src/main/java/com/instahipsta/harCRUD/repository/HarRepo.java
@@ -1,9 +1,9 @@
 package com.instahipsta.harCRUD.repository;
 
 import com.instahipsta.harCRUD.model.entity.Har;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface HarRepo extends CrudRepository<Har, Long> {
+public interface HarRepo extends JpaRepository<Har, Long> {
 
 }
