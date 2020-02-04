fe0c8ee59fae7c18f4aebc62e7bba3edc00553b3
instaHipsta282 <stepaden@mail.ru>
2020-02-04 13:36:46 +0300

    remove @import, testing

--- src/main/java/com/instahipsta/harCRUD/HarServiceApplication.java
+++ src/main/java/com/instahipsta/harCRUD/HarServiceApplication.java
@@ -1,11 +1,8 @@
 package com.instahipsta.harCRUD;
 
-import com.instahipsta.harCRUD.config.AppConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-@Import(AppConfiguration.class)
 @SpringBootApplication
 public class HarServiceApplication {
     public static void main(String[] args) {
